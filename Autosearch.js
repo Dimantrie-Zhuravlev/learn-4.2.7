edaa6e4dd57018e47484829314ed427cffa167fa
Dimantrie <tankerlord@mail.ru>
2022-06-13 23:17:29 +0300

    без удаление все работает

---
+++ Autosearch.js
@@ -0,0 +1,54 @@
+let input = document.querySelector('input');
+let arr = [];
+const debounce = (fn, debounceTime) => {
+    let time;
+    return function(...args) {
+        clearTimeout(time);
+        time = setTimeout(() => {
+            fn.apply(this, args)
+        }, debounceTime)
+    }
+};
+input.addEventListener('input', debounce(() => {
+
+    if (input.value) { getRepositories(input.value) }
+}, 500))
+
+function deleteElements(className) {
+    let arr = document.querySelectorAll(`.${className}`);
+    arr.forEach(elem => elem.remove())
+}
+
+function repElement(elemArr) {
+    const container = document.querySelector('.search__autoElements')
+    const fragment = document.createDocumentFragment();
+    let inputAuto = document.createElement('div');
+    const { name } = elemArr;
+    inputAuto.classList.add('inputSearch')
+    inputAuto.textContent = name;
+    inputAuto.setAttribute('data-id', arr.length - 1);
+    console.log(inputAuto.dataset.id)
+    fragment.appendChild(inputAuto)
+    container.appendChild(fragment);
+}
+
+async function getRepositories(inputText) {
+    deleteElements('inputSearch');
+    let elem = await fetch(`https://api.github.com/search/repositories?q=${inputText}`)
+    elem = await elem.json();
+    elem = elem.items;
+    arr = [];
+    if (elem.length) {
+        if (elem.length < 5) {
+            for (let i = 0; i < elem.length; i++) {
+                arr.push(elem[i]);
+                repElement(elem[i])
+            }
+        } else {
+            for (let i = 0; i < 5; i++) {
+                arr.push(elem[i]);
+                repElement(elem[i])
+            }
+        }
+    }
+}
---
+++ ClickRep.js
@@ -0,0 +1,29 @@
+let topTarget = document.querySelector('.search__autoElements'); //куда кликается
+function newPaint(newRepInfo) {
+    const container = document.querySelector('.result');
+    const fragment = document.createDocumentFragment();
+    let inputAuto = document.createElement('div');
+    const { name, stargazers_count: stars, owner } = arr[newRepInfo.dataset.id];
+    // console.log(name, stars, owner.login);
+    newParagraph(`Name: ${name}`, inputAuto);
+    newParagraph(`Owner: ${owner.login}`, inputAuto);
+    newParagraph(`Stars: ${stars}`, inputAuto);
+    //
+    inputAuto.classList.add('result__element')
+    fragment.appendChild(inputAuto)
+    container.appendChild(fragment);
+}
+
+function newParagraph(count, insertElement) {
+    let p = document.createElement('p');
+    p.textContent = count;
+    insertElement.appendChild(p);
+}
+
+function newParagraph()
+
+topTarget.addEventListener('click', (e) => {
+    console.log(arr);
+    console.log(e.target);
+    newPaint(e.target);
+})
